b619db8e3b36cb570f7b0952372d37bca14ea708
macbook <kriukov@pos-credir.ru>
2024-08-18 16:54:36 +0300

    draft

---
+++ main.py
@@ -0,0 +1,34 @@
+import time
+
+from src.api_hh import GetInfoHHCompany
+from src.create_DB import connect_to_db, create_tables, insert_employer, insert_vacancy
+from src.user_interface import display_data
+
+
+def main():
+    employer_ids = [1455, 3529, 2120, 4181, 2180, 80, 1740, 78638, 2537115, 3127]
+
+    conn = connect_to_db()
+    create_tables(conn)
+    cursor = conn.cursor()
+
+    hh_api = GetInfoHHCompany()
+
+    for employer_id in employer_ids:
+        time.sleep(1)
+        employer_info = hh_api.get_employer_info(employer_id)
+        if employer_info:
+            insert_employer(cursor, employer_info)
+            vacancies = hh_api.get_vacancies_by_employer(employer_id)
+            for vacancy in vacancies:
+                insert_vacancy(cursor, vacancy, employer_id)
+
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+    display_data()
+
+
+if __name__ == "__main__":
+    main()
